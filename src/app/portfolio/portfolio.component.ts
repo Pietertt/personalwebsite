9d4e1c5c0c5148fe87f8f6bd6687a53c81c951a5
Pieter Boersma <pieterboersma20@gmail.com>
2020-03-01 15:08:49 +0100

    Added .gitignore and upgraded the spinner

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from './../item.service';
 import { HttpClient } from '@angular/common/http';
+import { AppComponent } from '../app.component';
 
 import { IItem } from './../item.model';
 
@@ -13,6 +14,7 @@ import { IItem } from './../item.model';
 export class PortfolioComponent implements OnInit {
 
       items: IItem[] = [];
+      loading: boolean = false;
 
       constructor(private itemService: ItemService, private http: HttpClient) { }
 
@@ -21,10 +23,12 @@ export class PortfolioComponent implements OnInit {
       }
 
       getItems() {
+            this.loading = true;
             this.itemService.fetchItems(10).subscribe(data => {
                   for(let i = 0; i < data.length; i++){
                         this.items.push(data[i]);
                   }
+                  this.loading = false;
             });
       }
 
