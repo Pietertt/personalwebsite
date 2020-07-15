67b8ec6dce270c4465249efb3de3790dfb8fa8f5
Pieter Boersma <pieterboersma20@gmail.com>
2020-07-16 01:25:09 +0200

    Form doet t

--- plugins/pieterboersma/portfolio/controllers/Items.php
+++ plugins/pieterboersma/portfolio/controllers/Items.php
@@ -6,17 +6,25 @@
     use Backend\Classes\Controller;
 
     class Items extends Controller {
+        public $implement = ['Backend.Behaviors.FormController'];
+        public $formConfig = 'config_form.yaml';
 
         public function __construct()
         {   
             parent::__construct();
-
             BackendMenu::setContext('Pieterboersma.Portfolio', 'portfolio', 'items');
+
+            $this->pageTitle = 'Voeg portfolio items toe';
         }
     
         public function index()   
         {
-            return "Hoi";
+            $config = $this->makeConfig('$/pieterboersma/portfolio/models/item/fields.yaml');
+            $config->model = new \pieterboersma\portfolio\models\item;
+
+            $widget = $this->makeWidget('Backend\Widgets\Form', $config);
+
+            $this->vars['widget'] = $widget;
         }
     }
 ?>
